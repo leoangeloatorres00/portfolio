9d8b4db162cbd70592eb79fd660e5eb7e752a285
leoangeloatorres00 <37766005+leoangeloatorres00@users.noreply.github.com>
2023-09-18 15:09:22 +0800

    Initial dist subtree commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: "/portfolio/",
+  publicPath: "/portfolio/index",
   transpileDependencies: true,
 });
