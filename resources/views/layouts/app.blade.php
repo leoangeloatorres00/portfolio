04bb317e3fb70bfbb1d0fa6a2a802ed03855add7
Leo Angelo Torres <leoangeloatorres00@gmail.com>
2026-06-07 19:38:29 +0800

    update put height in mobile in desktop mode

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -153,9 +153,10 @@
 
             section.forEach(element => {
                 if(isMobileInDesktopMode()) {
-                    document.getElementById('isMobileInDesktopMode').innerHTML = "mobile desktop mode"
+                    document.getElementById('isMobileInDesktopMode').innerHTML = `mobile desktop mode ${window.outerHeight/2}px`
+                    element.classList.remove('min-h-[calc(100vh-64px)]');
                     
-                    element.style.height = window.outerHeight/2 + 'px';
+                    element.style.height = `${window.outerHeight/2}px`;
                 } else {
                     document.getElementById('isMobileInDesktopMode').innerHTML = "no mobile desktop mode"
 
